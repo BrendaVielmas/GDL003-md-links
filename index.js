61352f9e0d5c40944b27d6f721803084084e4738
Brenda Vielmas <breen.vielmas@gmail.com>
2019-08-15 18:48:43 -0500

    funciones resumidas ejemplo

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 
 let fs = require("fs");
+//Es directorio
+const itsDirectory = (path) => {
+	let resultOfDirectory = fs.lstatSync('./README.md').isDirectory()
 
-exports.mdLinks = (path, opts) => {
+	return resultOfDirectory;
 	
-	fs.readFile('./README.md', (err, data) => {
-		if (err) throw err;
-		console.log(data.toString())
-	});
-}
+};
+console.log(itsDirectory());
 
-exports.printMsg()
+//Es archivo
+const itsFile = (path) => {
+	let resultOfFile = fs.lstatSync('./README.md').isFile()
+
+	
+	console.log(itsFile());
+	if (resultOfFile == true) {
+		//Leer archivos
+		exports.mdLinks = (path, opts) => {
+	
+			fs.readFile('./README.md', (err, data) => {
+				if (err) throw err;
+				console.log(data.toString())
+			});
+		}
+		exports.mdLinks()
+		return resultOfFile;
+	} 
+	
+};
+
+
+/*Problema W: enseñar links validados
+Para resolver W necesito X: validar links
+Para resolver X necesito Y: encontrar links
+Para resolver Y necesito Z: leer un archivo md.
+Para resolver Z necesito A: filtrar archivos md.
+Para resolver A necesito B: leer directorio.
+Para resolver B necesito C: que sepa si es archivo o directorio.*/
+
+module.exports = (filePath) => {
+	if (path.extname(filePath) === ".md") {
+		return true;
+	}
+	return false;
+};
+
+
+// return path.extname(filePath) === ".md" ? true : false;
+
+//module.exports =(filePath) => path.extname(filepath) === ".md" ? true : false;
+
+//module.exports = filePath => path.extname(filepath) === ".md" ? true : false;
+
+//module.exports = filePath => path.extname(filepath) === ".md";
