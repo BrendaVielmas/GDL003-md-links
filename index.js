965bae2f3ec44e147d895e6ca4576cba3bcda51b
Brenda Vielmas <breen.vielmas@gmail.com>
2019-08-15 09:56:15 -0500

    Lectura de archivo

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-  // ...
-};
+
+let fs = require("fs");
+
+exports.mdLinks = (path, opts) => {
+	
+	fs.readFile('./README.md', (err, data) => {
+		if (err) throw err;
+		console.log(data.toString())
+	});
+}
+
+exports.printMsg()
